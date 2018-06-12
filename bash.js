e4f2c92e0c956e6227859623b94499a4a1f6e316
Ben Reitman <ben.reitman@gmail.com>
2018-06-12 13:54:36 -0400

    added ls

--- bash.js
+++ bash.js
@@ -2,11 +2,15 @@
 process.stdout.write('prompt > ');
 process.stdin.on('data', (data) =>{
     const cmd = data.toString().trim();
-    const pwd = require('./pwd.js')
+    const pwd = require('./pwd.js');
+    const ls = require('./ls.js');
     if(cmd === 'pwd'){
         pwd();
-    } else{    
-    process.stdout.write('You typed: ' + cmd);
     }
+    else if(cmd === 'ls'){
+        ls();
+    } else{
+    process.stdout.write('You typed: ' + cmd);
     process.stdout.write('\nprompt > ');
+    }
 });
